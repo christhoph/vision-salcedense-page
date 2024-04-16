54cd1dce262707295b3eada1be159d15215e1dd8
christhoph <cristoph.solis@outlook.com>
2024-04-16 17:31:36 -0500

    feat: create base layout for each page
    - extends tailwind styles based on project styles
    - create header component with logo and nav menu
    - create all pages with base layout

---
+++ src/utils/constants/menu.ts
@@ -0,0 +1,23 @@
+type MenuItem = {
+  route: string;
+  label: string;
+};
+
+export const MENU_ITEMS: MenuItem[] = [
+  {
+    route: "/",
+    label: "Inicio",
+  },
+  {
+    route: "/nosotros",
+    label: "Nosotros",
+  },
+  {
+    route: "/noticias",
+    label: "Noticias",
+  },
+  {
+    route: "/contactos",
+    label: "Contactos",
+  },
+];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#4F9E2D",
+        contrast: "#2A2B2C",
+      },
+    },
   },
   plugins: [],
 };
